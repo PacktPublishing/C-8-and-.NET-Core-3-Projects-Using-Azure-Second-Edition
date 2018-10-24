9d171a963230dcba228c57cb7d7fd5f8216e0e22
pcmichaels <pcmichaels@live.com>
2018-10-24 09:52:10 +0100

    Initial commit of the Signal R Chat Application

---
+++ Chapter 3 - SignalR Chat Application/RealTimeChat/Chat.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RealTimeChat
+{
+    public class Chat : Hub
+    {
+        public Task Send(string sender, string message) => 
+            Clients.All.SendAsync("UpdateChat", sender, message);
+
+        public Task ArchiveChat(string archivedBy, string path, string messages)
+        {
+            string fileName = "ChatArchive" +
+                DateTime.Now.ToString("yyyy_MM_dd_HH_mm") + ".txt";
+            System.IO.File.WriteAllText($@"{path}\{fileName}", messages);
+            return Clients.All.SendAsync("Archived", $"Chat archived by {archivedBy}"); 
+        }
+    }
+}
