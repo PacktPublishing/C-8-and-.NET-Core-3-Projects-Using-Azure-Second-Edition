52c8f131d095a4d565188a85163a6e4c2da71ab2
Paul Michaels <pcmichaels@live.com>
2019-01-13 10:23:47 +0000

    FileMonitor and PhotoService created

---
+++ Chapter 7 - Photo Storage/PhotoStorage.WindowsService/FileMonitor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace PhotoStorage.WindowsService
+{
+    internal class FileMonitor : IDisposable
+    {
+        private string _monitorPath;
+        private FileSystemWatcher _fileSystemWatcher;
+
+        public FileMonitor(string path)
+        {
+            _monitorPath = path;
+            _fileSystemWatcher = new FileSystemWatcher(path);
+
+            _fileSystemWatcher.Changed += new FileSystemEventHandler(OnChanged);
+            _fileSystemWatcher.Created += new FileSystemEventHandler(OnCreated);            
+            _fileSystemWatcher.Renamed += new RenamedEventHandler(OnRenamed);
+        }
+
+        private void OnRenamed(object sender, RenamedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnCreated(object sender, FileSystemEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            _fileSystemWatcher.Dispose();
+        }
+    }
+}
---
+++ Chapter 7 - Photo Storage/PhotoStorage.WindowsService/PhotoService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ServiceProcess;
+
+namespace PhotoStorage.WindowsService
+{
+    public class PhotoService : ServiceBase
+    {
+        private FileMonitor _fileMonitor;
+
+        protected override void OnStart(string[] args)
+        {
+            _fileMonitor = new FileMonitor("c:\tmp");
+        }
+
+        protected override void OnStop()
+        {
+            _fileMonitor.Dispose();
+        }
+    }
+}
